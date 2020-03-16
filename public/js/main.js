4d05a29c4ef730c51f2edd841cc5a6039208b758
ben_t44 <bethomas@westmont.edu>
2020-03-16 12:47:11 -0700

    added url parameters for lobby.html

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+/* functions for general use */
+
+
+/* This function returns the value associated with 'whichParam' on teh URL */
+function getUrlParameters(whichParam){
+    var pageURL = window.location.search.substring(1);
+    var pageURLVariables = pageURL.split('&');
+    for (var i = 0; i < pageURLVariables.length; i++){
+        var parameterName = pageURLVariables[i].split('=');
+        if(parameterName[0] == whichParam){
+            return parameterName[1];
+        }
+    }
+
+}
+
+var username = getUrlParameters('username');
+if('undefined' == typeof username || !username || username == ''){
+    username = 'Anonymous_'+Math.random();
+}
+$('#messages').append('<h4>'+username+'</h4>');
