e0d1fc884b47ba4f487e28660c5c8da20fd5172a
ben_t44 <bethomas@westmont.edu>
2020-04-06 21:34:54 -0700

    uninvite button

--- public/js/main.js
+++ public/js/main.js
@@ -131,7 +131,7 @@ socket.on('invite_response', function(payload){
         alert(payload.message);
         return;
     }
-    var newNode = makeInvitedButton();
+    var newNode = makeInvitedButton(payload.socket_id);
     $('.socket_'+payload.socket_id+' button').replaceWith(newNode);
     }
 );
@@ -145,6 +145,40 @@ socket.on('invited', function(payload){
     $('.socket_'+payload.socket_id+' button').replaceWith(newNode);
     }
 );
+
+/* Send an uninvite message to the server */
+function uninvite(who){
+    var payload = {};
+    payload.requested_user = who;
+
+    console.log('*** Client Log Message: \'uninvite\' payload: '+JSON.stringify(payload));
+    socket.emit('uninvite',payload);
+}
+
+socket.on('uninvite_response', function(payload){
+    if(payload.result == 'fail'){
+        alert(payload.message);
+        return;
+    }
+    var newNode = makeInviteButton(payload.socket_id);
+    $('.socket_'+payload.socket_id+' button').replaceWith(newNode);
+    }
+);
+
+/* Handle a notification that we have been univited */
+socket.on('uninvited', function(payload){
+    if(payload.result == 'fail'){
+        alert(payload.message);
+        return;
+    }
+    var newNode = makeInviteButton(payload.socket_id);
+    $('.socket_'+payload.socket_id+' button').replaceWith(newNode);
+    }
+);
+
+
+
+
 
 
 
@@ -173,9 +207,12 @@ function makeInviteButton(socket_id){
     return(newNode);
 }
 
-function makeInvitedButton(){
+function makeInvitedButton(socket_id){
     var newHTML = '<button type=\'button\' class=\'btn btn-primary\'>Invited</button>';
     var newNode = $(newHTML);
+    newNode.click(function(){
+        uninvite(socket_id);
+    });
     return(newNode);
 }
 
